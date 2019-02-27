596cf80df58cea8de7880d69a115ef0ccb2eb583
jotten7137 <jotten7137@gmail.com>
2019-02-26 20:34:00 -0500

    2019Feb26 commit

---
+++ app.py
@@ -0,0 +1,32 @@
+#dependencies
+import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','')
+
+db = SQLAlchemy(app)
+
+
+#Home Route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+#Query database and send jsonified results
+@app.route("/send",methods=)
+def send():
+    if request.method == "POST":
+        name = request.form['1']
+        
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
